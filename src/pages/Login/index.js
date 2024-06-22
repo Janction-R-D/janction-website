75b048d4909da4ada1d1973cdb6c1c1507ef7c9d
CanvasL <746591811@qq.com>
2024-06-22 15:50:41 +0800

    feat: login logic

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,11 +1,16 @@
 import { useConnectModal } from '@rainbow-me/rainbowkit';
 import { useModel, history } from 'umi';
+import { SiweMessage } from 'siwe';
 import styles from './index.less';
-import { useAccountEffect } from 'wagmi';
+import { useAccountEffect, useSignMessage } from 'wagmi';
 import storage from '@/utils/storage';
+import { fetchNonce, login } from '@/services/personal';
+import { useState } from 'react';
 
 const Login = (props) => {
   const { openConnectModal } = useConnectModal();
+  const { data, signMessageAsync } = useSignMessage();
+  const [token, setToken] = useState("");
   const { initialState, setInitialState } = useModel('@@initialState');
 
   useAccountEffect({
@@ -21,6 +26,42 @@ const Login = (props) => {
       storage.set({ name: 'userAccount', value: userAccount });
       const from = history.location.query?.from || '/';
       history.push(from);
+
+      const signAndLogin = async () => {
+        const nonce = await fetchNonce();
+        console.log("nonce:", nonce);
+
+        const siweMessage = new SiweMessage({
+          domain: 'janction.com',
+          address,
+          statement: 'Sign in Janction with your wallet.',
+          uri: 'https://janction.com',
+          version: '1',
+          chainId,
+          nonce,
+        });
+
+        const message = siweMessage.prepareMessage();
+
+        await signMessageAsync({
+          message,
+        }, {
+          onSuccess: async (data) => {
+            const param = {
+              message,
+              signature: data,
+            };
+    
+            console.log({param})
+    
+            const token = await login(param);
+            setToken(token);
+            console.log("token:", token);
+          }
+        });
+      };
+
+      signAndLogin();
     },
   });
 
