84cc48bf30194ba069cb5cc2068165e47058779f
Edondjiibolo <enzombul@gmail.com>
2024-09-29 18:56:56 +0800

    updated nodes page

--- src/utils/export.js
+++ src/utils/export.js
@@ -1,15 +1,12 @@
 import Papa from 'papaparse';
 // exportToCsv function is used to export data to csv file
 const exportToCsv = (columns, data) => {
-  // Crear un array para las cabeceras basado en las columnas
   const headers = columns.map((column) => column.title);
 
-  // Crear un array para los datos basado en las columnas y filas
   const csvData = data.map((row) => {
     return columns.map((column) => row[column.dataIndex]);
   });
 
-  // Incluir las cabeceras al principio del array de datos
   csvData.unshift(headers);
 
   const tsv = Papa.unparse(csvData, {
