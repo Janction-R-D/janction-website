5eee58126f80986390bb8b447640c6bd020d8b0e
WXL570CN <wxl570cn@163.com>
2024-10-18 23:27:39 +0800

    feat: lessors api

--- src/services/genesis/dashboard.js
+++ src/services/genesis/dashboard.js
@@ -1,3 +1,15 @@
 import { request } from 'umi';
 
 const baseUrl = '/v0';
+
+export const fetchLessor = async () => {
+  try {
+    const response = await request(`${baseUrl}/dashboard/lessor`, {
+      loginAuth: true,
+    });
+    return response;
+  } catch (error) {
+    console.log('『error』', error);
+    return null;
+  }
+};
