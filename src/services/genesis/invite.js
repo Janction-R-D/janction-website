57baab7d6c955d959f25ef980d8bb40cefc7ef11
WXL570CN <wxl570cn@163.com>
2024-11-03 16:28:57 +0800

    feat: invite api

---
+++ src/services/genesis/invite.js
@@ -0,0 +1,26 @@
+import { request } from 'umi';
+
+const baseUrl = '/v0';
+
+/**
+ *Generate invite link
+ */
+export const fetchInviteLink = async (body) => {
+  return request(`${baseUrl}/aff/generate`, {
+    method: 'POST',
+    body,
+    loginAuth: true,
+  });
+};
+
+/**
+ *Generate invite link
+ * @property {string} Query email
+ */
+export const fetchInviteSend = (data) => {
+  return request(`${baseUrl}/aff/send`, {
+    method: 'POST',
+    data,
+    loginAuth: true,
+  });
+};
