91875e862034ebb67a16ea1382c319d532f966a0
Edondjiibolo <enzombul@gmail.com>
2024-11-09 00:45:57 +0800

    merged

--- src/components/ProfileHeader.js
+++ src/components/ProfileHeader.js
@@ -58,7 +58,7 @@ function ProfileModal({ styles, classname, setShowModal }) {
             isLessees: !isLessees,
           });
           setShowModal(false);
-          // location.reload();
+          location.reload();
         };
         const handleLogOut = () => {
           disconnect();
