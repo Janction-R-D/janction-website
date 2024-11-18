e68ee5676157e14da8c3f0fe4c26a8d785f43d6a
Edondjiibolo <enzombul@gmail.com>
2024-11-18 16:41:52 +0800

    some changes on user-center

--- src/pages/Genesis/User-center/components/EditName.js
+++ src/pages/Genesis/User-center/components/EditName.js
@@ -20,7 +20,7 @@ function Edit({ handleCancel, isModalOpen, handleOk, setName, name }) {
     >
       <header>
         <p>Edit Username</p>
-        <i className="iconfont icon-close"></i>
+        <i className="iconfont icon-close" onClick={handleCancel}></i>
       </header>
       <Input
         className={styles['input-name']}
