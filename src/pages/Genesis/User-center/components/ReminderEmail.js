247c1df634522a6625fd2d2baad819074337bb4a
Edondjiibolo <enzombul@gmail.com>
2024-11-21 23:40:50 +0800

    changes email verify

--- src/pages/Genesis/User-center/components/ReminderEmail.js
+++ src/pages/Genesis/User-center/components/ReminderEmail.js
@@ -13,7 +13,7 @@ function Remind({
   const [isEmailConfigOpen, setIsEmailConfigOpen] = useState(false);
   const showEmailConf = () => {
     setIsEmailConfigOpen(true);
-    handleOk();
+    // handleOk();
   };
   return (
     <Modal
