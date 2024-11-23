6259be16b58d7df7737106a9a490bb7c5c9e928f
WXL570CN <wxl570cn@163.com>
2024-11-23 15:02:15 +0800

    feat: production proxy

--- config/proxy.js
+++ config/proxy.js
@@ -9,4 +9,14 @@ export default {
       changeOrigin: true,
     },
   },
+  uat: {
+    '/api/': {
+      target: 'https://www.janction.io/',
+      changeOrigin: true,
+    },
+    '/v0/': {
+      target: 'http://18.181.196.49:8080/',
+      changeOrigin: true,
+    },
+  },
 };
