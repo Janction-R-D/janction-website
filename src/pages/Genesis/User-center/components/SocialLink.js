d25bcbf2fe601139455716848deefdfd522515be
Edondjiibolo <enzombul@gmail.com>
2024-12-04 03:43:25 +0800

    link social media ui updated discord linked

--- src/pages/Genesis/User-center/components/SocialLink.js
+++ src/pages/Genesis/User-center/components/SocialLink.js
@@ -1,12 +1,210 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { Card, Button } from 'antd';
+import { request } from 'umi';
 import styles from '../index.less';
-import { Button, Card } from 'antd';
+
+async function getDiscordUserData(token) {
+  try {
+    const data = await request('https://discord.com/api/v10/users/@me', {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    console.log('User data from Discord:', data);
+    return data;
+  } catch (error) {
+    console.error('Error fetching Discord user data:', error);
+  }
+}
+
+async function getTwitterUserData(token) {
+  try {
+    const data = await request('https://api.twitter.com/2/me', {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    console.log('User data from Twitter:', data);
+    return data;
+  } catch (error) {
+    console.error('Error fetching Twitter user data:', error);
+  }
+}
+
+function DiscordElement({ icon }) {
+  const [isConnected, setIsConnected] = useState(false);
+  const [token, setToken] = useState(null);
+  const [userInfo, setUserInfo] = useState(null);
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(
+      window.location.hash.replace('#', '?'),
+    );
+    const accessToken = urlParams.get('access_token');
+
+    if (accessToken) {
+      setToken(accessToken);
+      localStorage.setItem('discordToken', accessToken);
+      setIsConnected(true);
+
+      getDiscordUserData(accessToken).then((data) => {
+        setUserInfo(data);
+      });
+    } else {
+      const storedToken = localStorage.getItem('discordToken');
+      if (storedToken) {
+        setToken(storedToken);
+        setIsConnected(true);
+        getDiscordUserData(storedToken).then((data) => {
+          setUserInfo(data);
+        });
+      }
+    }
+  }, []);
+
+  const handleClick = () => {
+    if (isConnected) {
+      setToken(null);
+      localStorage.removeItem('discordToken');
+      setIsConnected(false);
+      setUserInfo(null);
+    } else {
+      redirectToDiscordAuth();
+    }
+  };
+
+  const redirectToDiscordAuth = () => {
+    const clientId = '1313569428213862490';
+    const redirectUri = encodeURIComponent(
+      'http://localhost:8000/genesis/user-center',
+    );
+    const scopes = 'identify';
+    const responseType = 'token';
+    const discordAuthUrl = `https://discord.com/api/oauth2/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=${responseType}&scope=${scopes}`;
+    window.open(discordAuthUrl, '_blank');
+  };
+
+  return (
+    <article className={styles['social-tag']}>
+      <div className={styles['social-box']}>
+        <div className={styles['social-icon']}>
+          <i className={`iconfont icon-${icon}`}></i>
+        </div>
+        {isConnected ? (
+          <p>{userInfo?.username}</p>
+        ) : (
+          <p>Connect to Discord account</p>
+        )}
+
+        {isConnected && (
+          <div className={styles['isConnected']}>
+            <i className="iconfont icon-check"></i>
+            <p>Connected</p>
+          </div>
+        )}
+      </div>
+      <div className={styles['buttons']}>
+        <Button
+          className={isConnected ? styles['cancel-btn'] : styles['create-btn']}
+          onClick={handleClick}
+        >
+          {isConnected ? 'Disconnect' : 'Connect'}
+        </Button>
+      </div>
+    </article>
+  );
+}
+
+function TwitterElement({ icon }) {
+  const [isConnected, setIsConnected] = useState(false);
+  const [token, setToken] = useState(null);
+  const [userInfo, setUserInfo] = useState(null);
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(
+      window.location.hash.replace('#', '?'),
+    );
+    const accessToken = urlParams.get('access_token');
+
+    if (accessToken) {
+      setToken(accessToken);
+      localStorage.setItem('twitterToken', accessToken);
+      setIsConnected(true);
+
+      getTwitterUserData(accessToken).then((data) => {
+        setUserInfo(data);
+      });
+    } else {
+      const storedToken = localStorage.getItem('twitterToken');
+      if (storedToken) {
+        setToken(storedToken);
+        setIsConnected(true);
+        getTwitterUserData(storedToken).then((data) => {
+          setUserInfo(data);
+        });
+      }
+    }
+  }, []);
+
+  const handleClick = () => {
+    if (isConnected) {
+      setToken(null);
+      localStorage.removeItem('twitterToken');
+      setIsConnected(false);
+      setUserInfo(null);
+    } else {
+      redirectToTwitterAuth();
+    }
+  };
+
+  const redirectToTwitterAuth = () => {
+    const clientId = 'NDhuSVlLaXlSWXhvdVhOU2hSLVo6MTpjaQ';
+    const redirectUri = encodeURIComponent(
+      'http://localhost:8000/genesis/user-center',
+    );
+    const scopes = 'tweet.read users.read';
+    const responseType = 'token';
+    const twitterAuthUrl = `https://twitter.com/i/oauth2/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=${responseType}&scope=${scopes}`;
+    window.open(twitterAuthUrl, '_blank');
+  };
+
+  return (
+    <article className={styles['social-tag']}>
+      <div className={styles['social-box']}>
+        <div className={styles['social-icon']}>
+          <i className={`iconfont icon-${icon}`}></i>
+        </div>
+        {isConnected ? (
+          <p>{userInfo?.username}</p>
+        ) : (
+          <p>Connect to Twitter account</p>
+        )}
+
+        {isConnected && (
+          <div className={styles['isConnected']}>
+            <i className="iconfont icon-check"></i>
+            <p>Connected</p>
+          </div>
+        )}
+      </div>
+      <div className={styles['buttons']}>
+        <Button
+          className={isConnected ? styles['cancel-btn'] : styles['create-btn']}
+          onClick={handleClick}
+        >
+          {isConnected ? 'Disconnect' : 'Connect'}
+        </Button>
+      </div>
+    </article>
+  );
+}
+
 export default function SocialLink() {
-  const [discordConnect, setdiscordConnect] = useState(false);
-  const [xConnect, setxConnect] = useState(true);
   const icons = {
-    x: 'x',
     discord: 'discord',
+    twitter: 'x',
   };
 
   return (
@@ -15,51 +213,9 @@ export default function SocialLink() {
         <h3>Link your social tags</h3>
       </section>
       <section className={styles['social-tags']}>
-        <SociaElement
-          isConneted={xConnect}
-          setIsconnect={setxConnect}
-          social={'Twitter'}
-          icon={icons.x}
-        />
-        <SociaElement
-          isConneted={discordConnect}
-          setIsconnect={setdiscordConnect}
-          social={'Discord'}
-          icon={icons.discord}
-        />
+        <TwitterElement icon={icons.twitter} />
+        <DiscordElement icon={icons.discord} />
       </section>
     </Card>
-  );
-}
-
-function SociaElement({ isConneted, social, setIsconnect, icon }) {
-  console.log(icon);
-  return (
-    <article className={styles['social-tag']}>
-      <div className={styles['social-box']}>
-        <div className={styles['social-icon']}>
-          <i className={`iconfont icon-${icon}`}></i>
-        </div>
-        {isConneted == false ? (
-          <p>Connect to {social} account</p>
-        ) : (
-          <p> {social} Account Name</p>
-        )}
-
-        {isConneted && (
-          <div className={styles['isConnected']}>
-            <i className="iconfont icon-check"></i>
-            <p>Connected</p>
-          </div>
-        )}
-      </div>
-      <div className={styles['buttons']}>
-        {isConneted == false ? (
-          <Button className={styles['create-btn']}>Connect</Button>
-        ) : (
-          <Button className={styles['cancel-btn']}>Disconnect</Button>
-        )}
-      </div>
-    </article>
   );
 }
