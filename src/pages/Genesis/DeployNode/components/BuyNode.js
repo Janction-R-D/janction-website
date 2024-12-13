3866b35a5191cedf817738475113f86b246a1228
Edondjiibolo <enzombul@gmail.com>
2024-12-14 03:42:59 +0800

    purchase order deploy node

--- src/pages/Genesis/DeployNode/components/BuyNode.js
+++ src/pages/Genesis/DeployNode/components/BuyNode.js
@@ -1,15 +1,31 @@
 import React, { useEffect, useState } from 'react';
-import welcome from '@/assets/images/home/welcome.png';
+import buy from '@/assets/images/genesis/buy.png';
+import purchase from '@/assets/images/genesis/purchase.png';
 import { Button, Input, Modal } from 'antd';
 import styles from './node.less';
 export default function BuyNode({ item }) {
-  const [isOpen, setIsOpen] = useState(true);
-  const [email, setEmail] = useState('');
+  const [isOpen, setIsOpen] = useState(false);
+  const [isPay, setIsPay] = useState(true);
+
   const handleOk = () => {
     setIsOpen(true);
   };
+  const handleOkPay = () => {
+    setIsPay(true);
+  };
   const handleCancel = () => {
     setIsOpen(false);
+  };
+  const handleCancelPay = () => {
+    setIsPay(false);
+  };
+  const handlePay = () => {
+    setIsOpen(false);
+
+    setTimeout(() => {
+      setIsPay(true);
+      console.log(isPay);
+    }, 500);
   };
 
   return (
@@ -17,7 +33,12 @@ export default function BuyNode({ item }) {
       <div className={styles['btn']} onClick={handleOk}>
         Buy Now!
       </div>
-
+      <PayCaard
+        isPay={isPay}
+        setIsPay={setIsPay}
+        handleCancelPay={handleCancelPay}
+        handleOkPay={handleOkPay}
+      />
       <Modal
         open={isOpen}
         onOk={handleOk}
@@ -27,7 +48,7 @@ export default function BuyNode({ item }) {
         footer={false}
       >
         <div className={styles['modal-img']}>
-          <img src={welcome} />
+          <img src={buy} />
         </div>
         <section className={styles['modal-info']}>
           <h2>Buy Janction Node</h2>
@@ -40,7 +61,10 @@ export default function BuyNode({ item }) {
             <p>1,000 USDT</p>
           </div>
           <div>
-            <Button className={styles['buy-btn']}>Click to pay</Button>
+            <Button className={styles['buy-btn']} onClick={handlePay}>
+              Click to pay
+            </Button>
+
             <p className={styles['text-grey']}>
               Surrender your rights, <span>Enter immediately</span>
             </p>
@@ -48,5 +72,59 @@ export default function BuyNode({ item }) {
         </section>
       </Modal>
     </>
+  );
+}
+
+function PayCaard({ isPay, setIsPay, handleCancelPay, handleOkPay }) {
+  const handlePay = () => {
+    setIsPay(true);
+  };
+
+  return (
+    <Modal
+      open={isPay}
+      onOk={handleOkPay}
+      onCancel={handleCancelPay}
+      className={styles['modal-purchase']}
+      width={900}
+      footer={false}
+    >
+      <div className={styles['modal-img']}>
+        <img src={purchase} />
+        <section>
+          <p>
+            <i className="iconfont icon-list"></i> Details
+          </p>
+          <ul>
+            <li>
+              <p>Status:</p>
+              <p>Complete</p>
+            </li>
+            <li>
+              <p>Transaction Hash:</p>
+              <p>0xe3802293</p>
+            </li>
+            <li>
+              <p>ID:</p>
+              <p>73489024hu094invm</p>
+            </li>
+            <li>
+              <p>Contract address:</p>
+              <p>4678ghrtcgmgc</p>
+            </li>
+          </ul>
+        </section>
+      </div>
+      <section className={styles['modal-info']}>
+        <h2>Your purchase has been processed! </h2>
+        <p>Congratulations on joining the Janction Contributor Network!</p>
+
+        <div>
+          <Button className={styles['buy-btn']} onClick={handlePay}>
+            Check rewards
+          </Button>
+        </div>
+      </section>
+    </Modal>
   );
 }
