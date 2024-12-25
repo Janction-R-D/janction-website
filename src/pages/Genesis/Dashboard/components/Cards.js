abad8c111824a728adaa88c6c80e541c82f482b8
Edondjiibolo <enzombul@gmail.com>
2024-12-26 05:54:10 +0800

    carrusel coverFlow

--- src/pages/Genesis/Dashboard/components/Cards.js
+++ src/pages/Genesis/Dashboard/components/Cards.js
@@ -1,77 +1,106 @@
-import React, { useRef, useState } from 'react';
-// Import Swiper React components
+import React, { useState } from 'react';
+import slide1 from '@/assets/images/genesis/ntf/silde-1.png';
+import slide2 from '@/assets/images/genesis/ntf/silde-3.png';
+import slide3 from '@/assets/images/genesis/ntf/silde-4.png';
+import slide4 from '@/assets/images/genesis/ntf/silde-5.png';
+import slide5 from '@/assets/images/genesis/ntf/silde-6.png';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import img from '@/assets/images/genesis/ntf/silde-1.png';
 import { Modal } from 'antd';
 import 'swiper/css';
 import 'swiper/css/effect-cards';
 import styles from './index.less';
 import './cards.less';
-
-// import required modules
+import { EffectCoverflow, Pagination } from 'swiper/modules';
 import { EffectCards } from 'swiper/modules';
-const sliderElements = [
+
+const slidesData = [
   {
     key: 1,
-    image: '@/assets/images/genesis/ntf/silde-1.png',
+    image: slide1,
     address: '0x1234567890abcdef1234567890abcdef12345678',
   },
   {
     key: 2,
-    image: '@/assets/images/genesis/ntf/silde-3.png',
+    image: slide2,
     address: '0xabcdef1234567890abcdef1234567890abcdef12',
   },
   {
     key: 3,
-    image: '@/assets/images/genesis/ntf/silde-4.png',
+    image: slide3,
     address: '0x7890abcdef1234567890abcdef1234567890abcd',
   },
-  //   {
-  //     key: 4,
-  //     image: '@/assets/images/genesis/ntf/silde-5.png',
-  //     address: '0x4567890abcdef1234567890abcdef1234567890a',
-  //   },
-  //   {
-  //     key: 5,
-  //     image: '@/assets/images/genesis/ntf/silde-6.png',
-  //     address: '0xabcdef7890abcdef1234567890abcdef1234567b',
-  //   },
+  {
+    key: 4,
+    image: slide4,
+    address: '0x4567890abcdef1234567890abcdef1234567890a',
+  },
+  {
+    key: 5,
+    image: slide5,
+    address: '0xabcdef7890abcdef1234567890abcdef1234567b',
+  },
 ];
 
 export default function Cards() {
-  const [isPay, setIsPay] = useState(false);
+  const [selectedNtf, setSelectedNtf] = useState(null); // Nuevo estado para almacenar el NFT seleccionado
+
   const handleOk = () => {
-    setIsPay(true);
+    setSelectedNtf(null); // Cierra el modal al hacer clic en 'Ok'
   };
+
   const handleCancelPay = () => {
-    setIsPay(false);
+    setSelectedNtf(null); // Cierra el modal al cancelar
   };
+
+  const handleClick = (ntf) => {
+    setSelectedNtf(ntf); // Establece el NFT seleccionado al hacer clic en la imagen
+  };
+
   return (
     <>
       <Swiper
-        effect={'cards'}
+        effect={'coverflow'}
         grabCursor={true}
-        modules={[EffectCards]}
+        centeredSlides={true}
+        slidesPerView={'auto'}
+        coverflowEffect={{
+          rotate: 10,
+          stretch: 0,
+          depth: 500,
+          modifier: 1,
+          slideShadows: false,
+        }}
+        pagination={true}
+        modules={[EffectCoverflow, Pagination]}
         className="mySwiper"
       >
-        {sliderElements.map((ntf) => (
+        {slidesData.map((ntf, index) => (
           <SwiperSlide key={ntf.key}>
-            <img src={img} onClick={handleOk} />
-            <MyNtf
-              handleOk={handleOk}
-              handleCancel={handleCancelPay}
-              isPay={isPay}
+            <img
+              src={ntf.image}
+              alt={`NFT ${index + 1}`}
+              onClick={() => handleClick(ntf)} // Llama a handleClick con el NFT correspondiente
             />
           </SwiperSlide>
         ))}
       </Swiper>
+
+      {/* Mostrar el modal solo si hay un NFT seleccionado */}
+      {selectedNtf && (
+        <MyNtf
+          handleOk={handleOk}
+          handleCancel={handleCancelPay}
+          data={selectedNtf} // Pasa el NFT seleccionado
+        />
+      )}
     </>
   );
 }
-function MyNtf({ isPay, handleCancel, handleOk }) {
+
+function MyNtf({ handleOk, handleCancel, data }) {
   return (
     <Modal
-      open={isPay}
+      open={true} // El modal siempre está abierto cuando hay un NFT seleccionado
       onOk={handleOk}
       onCancel={handleCancel}
       className={styles['modal-purchase']}
@@ -80,7 +109,7 @@ function MyNtf({ isPay, handleCancel, handleOk }) {
       closable={false}
     >
       <div className={styles['modal-img']}>
-        <img src={img} />
+        <img src={data.image} alt="NFT Selected" />
         <section>
           <p>
             <i className="iconfont icon-list"></i> Details
@@ -92,11 +121,11 @@ function MyNtf({ isPay, handleCancel, handleOk }) {
             </li>
             <li>
               <p>Transaction Hash:</p>
-              <p>0xe3802293</p>
+              <p>{data.address}</p>
             </li>
             <li>
               <p>ID:</p>
-              <p>73489024hu094invm</p>
+              <p>{data.key}</p>
             </li>
             <li>
               <p>Contract address:</p>
--- src/pages/Genesis/Dashboard/components/NTFcard.js
+++ src/pages/Genesis/Dashboard/components/NTFcard.js
@@ -1,83 +1,15 @@
 import React, { useEffect, useState } from 'react';
-import Swiper from 'swiper';
-import img from '@/assets/images/genesis/ntf/silde-1.png';
+
 import banner from '@/assets/images/genesis/banner-8.png';
-import { Button, Modal } from 'antd';
-import { Navigation, Pagination } from 'swiper/modules';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
 import styles from './index.less';
 import Cards from './Cards';
 
-const sliderElements = [
-  {
-    key: 1,
-    image: '@/assets/images/genesis/ntf/silde-1.png',
-    address: '0x1234567890abcdef1234567890abcdef12345678',
-  },
-  {
-    key: 2,
-    image: '@/assets/images/genesis/ntf/silde-3.png',
-    address: '0xabcdef1234567890abcdef1234567890abcdef12',
-  },
-  {
-    key: 3,
-    image: '@/assets/images/genesis/ntf/silde-4.png',
-    address: '0x7890abcdef1234567890abcdef1234567890abcd',
-  },
-  //   {
-  //     key: 4,
-  //     image: '@/assets/images/genesis/ntf/silde-5.png',
-  //     address: '0x4567890abcdef1234567890abcdef1234567890a',
-  //   },
-  //   {
-  //     key: 5,
-  //     image: '@/assets/images/genesis/ntf/silde-6.png',
-  //     address: '0xabcdef7890abcdef1234567890abcdef1234567b',
-  //   },
-];
-
 const NTFcard = () => {
-  //   useEffect(() => {
-  //     const swiper = new Swiper('.swiper-container', {
-  //       // Optional parameters
-  //       direction: 'horizontal',
-  //       loop: true,
-  //       effect: 'coverFlow',
-  //       grabCursor: true,
-  //       centeredSlides: true,
-  //       slidesPerView: 'auto',
-  //       spaceBetween: 20,
-  //       initialSlide: 1,
-  //       coverflowEffect: {
-  //         rotate: 0,
-  //         stretch: 80,
-  //         depth: 200,
-  //         modifier: 1,
-  //         slideShadows: true,
-  //       },
-  //       // If we need pagination
-  //       pagination: {
-  //         el: `.${styles['swiper-pagination']}`,
-  //         clickable: true,
-  //       },
-
-  //       // Navigation arrows
-  //       navigation: {
-  //         nextEl: '.swiper-button-next',
-  //         prevEl: '.swiper-button-prev',
-  //       },
-
-  //       modules: [Navigation, Pagination],
-  //     });
-  //   }, []);
-
   return (
     <div
       className={`swiper-container ${styles['sales-wrapper']} ${styles['swiper-container']}  `}
     >
+      <h2>My NFT</h2>
       <img className={styles['banner-img']} src={banner} />
       <Cards />
     </div>
