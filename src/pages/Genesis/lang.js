a0d33d59f05e85f006c6e43a93750d1d62ea8539
巫锡良 <42182003+WXL570CN@users.noreply.github.com>
2024-12-31 13:47:46 +0800

    fix: Handle BigNumber error in decimal cases

--- src/pages/Genesis/lang.js
+++ src/pages/Genesis/lang.js
@@ -2,8 +2,9 @@ import { ethers } from 'ethers';
 
 export const toFixed = (value, decimals) => {
   if (isNaN(value)) return 0;
-  // 假设 formatEther 输出的值
-  const weiValue = ethers.BigNumber.from(value);
+  // 处理输入，确保 value 是有效的整数字符串（含小数点会报错）
+  const stringValue = value.toString().split('.')[0];
+  const weiValue = ethers.BigNumber.from(stringValue);
   const etherValue = ethers.utils.formatEther(weiValue); // 转换为 Ether
   // 转换为 Number 再用 toFixed
   const roundedValue = Number(etherValue).toFixed(decimals); // 保留两位小数，四舍五入
