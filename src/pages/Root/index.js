961eaffbe21f212af55c2f806d69bb49ee0a60ba
WXL570CN <wxl570cn@163.com>
2025-01-08 01:09:58 +0800

    fix: remove invalid import

--- src/pages/Root/index.js
+++ src/pages/Root/index.js
@@ -24,7 +24,6 @@ import SplitRatioSetting from './components/SplitRatioSetting';
 import StatisticCard from './components/StatisticCard';
 import styles from './index.less';
 import ModifyModal from './components/ModifyModal';
-import { update } from 'lodash';
 
 const Root = (props) => {
   const [record, setRecord] = useState();
