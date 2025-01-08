fd7305d5420469df299b21292bdfeff3f9563e1a
WXL570CN <wxl570cn@163.com>
2025-01-08 22:04:53 +0800

    feat: prod api

--- config/config.prod.js
+++ config/config.prod.js
@@ -4,8 +4,8 @@ export default defineConfig({
   define: {
     'process.env': {
       NODE_ENV: 'production',
-      JANCTION_API: '/api',
-      JANCTION_V0_API: '/v0',
+      JANCTION_API: 'https://api.janction.io/api',
+      JANCTION_V0_API: 'https://api.janction.io/v0',
       REGISTER_NODE_URL: 'https://api.janction.io',
     },
   },
