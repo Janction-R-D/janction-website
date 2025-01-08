3cee589bda6d86433a4d06632dc02cd80ff1147d
WXL570CN <wuxiliang@yagena.net>
2025-01-08 14:43:29 +0800

    feat: test api update

--- config/config.test.js
+++ config/config.test.js
@@ -4,8 +4,8 @@ export default defineConfig({
   define: {
     'process.env': {
       NODE_ENV: 'test',
-      JANCTION_API: 'http://54.95.234.135:81/api',
-      JANCTION_V0_API: 'http://54.95.234.135:81/v0',
+      JANCTION_API: 'http://dev.janction.io/api',
+      JANCTION_V0_API: 'http://dev.janction.io/v0',
       REGISTER_NODE_URL: 'http://dev.janction.io',
     },
   },
