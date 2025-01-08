5a470e374b6968c64fb7855bca9b25cc99847db7
WXL570CN <wxl570cn@163.com>
2025-01-08 21:12:46 +0800

    fix: test api update

--- config/config.test.js
+++ config/config.test.js
@@ -4,9 +4,9 @@ export default defineConfig({
   define: {
     'process.env': {
       NODE_ENV: 'test',
-      JANCTION_API: 'http://dev.janction.io/api',
-      JANCTION_V0_API: 'http://dev.janction.io/v0',
-      REGISTER_NODE_URL: 'http://dev.janction.io',
+      JANCTION_API: 'https://dev.janction.io/api',
+      JANCTION_V0_API: 'https://dev.janction.io/v0',
+      REGISTER_NODE_URL: 'https://dev.janction.io',
     },
   },
 });
