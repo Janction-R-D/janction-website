d759143c1c28ed18bd3c2fb851bd16d7b4c5f920
WXL570CN <wxl570cn@163.com>
2025-01-14 01:41:30 +0800

    feat: user center data edit

--- src/pages/Genesis/User-center/components/UserAssets.js
+++ src/pages/Genesis/User-center/components/UserAssets.js
@@ -1,87 +1,73 @@
-import React, { useEffect, useState } from 'react';
-import { Card, Input, Select, Button } from 'antd';
-import styles from '../index.less';
 import JanctionTip from '@/components/JanctionTip';
+import { sendImageToServer } from '@/services/genesis';
+import { Button, Card, Form, InputNumber, message } from 'antd';
+import { isEmpty } from 'lodash';
+import { useEffect } from 'react';
+import styles from '../index.less';
+
 export default function UserAssets({ data, duration, setDuration }) {
-  const options = [
-    { value: 1, label: '1 Month' },
-    { value: 2, label: '2 Months' },
-    { value: 3, label: '3 Months' },
-    { value: 4, label: '4 Months' },
-    { value: 5, label: '5 Months' },
-    { value: 6, label: '6 Months' },
-    { value: 9, label: '9 Months' },
-    { value: 12, label: '1 Year' },
-  ];
+  const [form] = Form.useForm();
+
   useEffect(() => {
-    console.log(data);
-    setDuration(options[0]);
-  }, []);
-  const handleChange = (e) => {
-    console.log(e);
+    if (isEmpty(data?.assets)) return;
+    form.setFieldsValue(data?.assets);
+  }, [data]);
+
+  const onSubmit = async () => {
+    try {
+      const assets = await form.validateFields();
+      await sendImageToServer({ assets });
+      message.success('Update success!');
+    } catch (err) {
+      console.log('『err』', err);
+      message.error('Update failed, please try again!');
+    }
   };
+
   return (
     <Card className={styles['card']}>
       <section className={styles['card-header']}>
         <h3>Staking your assets</h3>
         <JanctionTip title="Bet your empty currency to earn rewards and help maintain network security." />
       </section>
-      <section className={styles['card-assets-items']}>
-        <div>
-          <p>Quantity pledged (USDT)</p>
-          <Input
-            type="number"
-            inputMode="numeric"
-            className={styles['card-assets-input']}
-            defaultValue={data?.assets?.amount || 0}
-            suffix="USDT"
-            name="amount"
-          />
-          {/* <section className={styles['card-assets-input']}>
-            <p> {data?.assets?.amount}</p>
-            <span>ETH</span>
-          </section> */}
-        </div>
-        <div>
-          <p>Duration pledged (Months)</p>
-          <Select
-            className={styles['card-assets-select']}
-            options={options}
-            defaultValue={{
-              value: data.assets?.duration_months || 0,
-              label: `${data.assets?.duration_months || 0} ${
-                data.assets?.duration_months !== 1 ? 'Months' : 'Month'
-              }`,
-            }}
-            bordered={false}
-            onChange={handleChange}
-            fieldNames="duration_months"
-            name="duration_months"
-          />
-          {/* <section className={styles['card-assets-input']}>
-            {data.assets?.duration_months}{' '}
-            {data.assets?.duration_months > 1 ? 'Months' : 'Month'}
-          </section> */}
-        </div>
-        <div>
-          <p>Anticipated income</p>
-          <Input
-            type="number"
-            className={styles['card-assets-input']}
-            defaultValue={data?.assets?.anticipated_income || 0}
-            suffix="USDT"
-            name="anticipated_income"
-          />
-          {/* <section className={styles['card-assets-input']}>
-            <p>{data?.assets?.anticipated_income}</p>
-            <span>ETH</span>
-          </section> */}
-        </div>
-      </section>
+      <Form form={form} initialValues={data?.assets}>
+        <section className={styles['card-assets-items']}>
+          <div>
+            <p>Quantity pledged (USDT)</p>
+            <Form.Item noStyle name="amount">
+              <InputNumber
+                min={0}
+                className={styles['card-assets-input']}
+                addonAfter="USDT"
+              />
+            </Form.Item>
+          </div>
+          <div>
+            <p>Duration pledged (Months)</p>
+            <Form.Item noStyle name="duration_months">
+              <InputNumber
+                min={0}
+                className={styles['card-assets-input']}
+                addonAfter="Months"
+              />
+            </Form.Item>
+          </div>
+          <div>
+            <p>Anticipated income</p>
+            <Form.Item noStyle name="anticipated_income">
+              <InputNumber
+                min={0}
+                className={styles['card-assets-input']}
+                addonAfter="USDT"
+              />
+            </Form.Item>
+          </div>
+        </section>
+      </Form>
       <Button
         className={styles['create-btn']}
         style={{ paddingInline: '28px', marginTop: '12px' }}
-        htmlType="submit"
+        onClick={onSubmit}
       >
         Save
       </Button>
