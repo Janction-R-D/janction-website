a55c3fe7f0f638ed23e5fc2738089dffc2040a94
mac <mac@Monomoskin-MacBook-Pro.local>
2025-01-16 17:09:44 +0800

    feat:some changes in message center table responsive

--- src/pages/Genesis/Message-center/components/CheckHeader.js
+++ src/pages/Genesis/Message-center/components/CheckHeader.js
@@ -1,6 +1,5 @@
 import { Button, Checkbox } from 'antd';
-import styles from '../index.less';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 export function CheckHeader({
   filteredMessages,
@@ -8,6 +7,7 @@ export function CheckHeader({
   setIsChecked,
   isChecked,
 }) {
+  const [isAllcheck, setIsAllCheck] = useState(false);
   const setAsReadedMessages = () => {
     const selectedMessages = filteredMessages.filter(
       (message) => message.checked === true,
@@ -33,18 +33,31 @@ export function CheckHeader({
     if (selectedMessages.length <= 0) return;
     setAllMessages(selectedMessages);
   };
+  const checkIsAllChecked = () => {
+    const filterChecked = filteredMessages.filter(
+      (sms) => sms.checked === false,
+    );
+
+    if (filterChecked.length > 0) {
+      setIsAllCheck(false);
+      return;
+    }
+    setIsAllCheck(true);
+  };
   useEffect(() => {
     const findIsChecked = filteredMessages.findIndex(
       (message) => message.checked === true,
     );
-
+    checkIsAllChecked();
     if (findIsChecked !== -1) {
       setIsChecked(true);
+
       return;
     } else {
       setIsChecked(false);
     }
   }, [filteredMessages]);
+
   const handleClick = () => {
     if (isChecked) {
       const newMessages = filteredMessages.map((sms) => ({
@@ -62,10 +75,18 @@ export function CheckHeader({
   };
   return (
     <div className="table-header">
-      <Checkbox checked={isChecked} onClick={handleClick} />
+      <Checkbox
+        checked={isChecked}
+        onClick={handleClick}
+        className={`${isAllcheck ? '' : 'isCheckSome'}`}
+      />
       <div>
-        <Button onClick={deleteMessages}>Delete</Button>
-        <Button onClick={setAsReadedMessages}>Mark as Read</Button>
+        <Button onClick={deleteMessages} disabled={!isChecked}>
+          Delete
+        </Button>
+        <Button onClick={setAsReadedMessages} disabled={!isChecked}>
+          Mark as Read
+        </Button>
       </div>
     </div>
   );
