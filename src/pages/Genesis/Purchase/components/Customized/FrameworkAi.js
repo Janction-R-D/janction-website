1466f3d1f653f9185bd3c9a1ff44699b7ef5770a
WXL570CN <wxl570cn@163.com>
2025-04-03 16:58:58 +0800

    style: resource

--- src/pages/Genesis/Purchase/components/Customized/FrameworkAi.js
+++ src/pages/Genesis/Purchase/components/Customized/FrameworkAi.js
@@ -43,7 +43,7 @@ export default function FrameworkAi({ value, onChange }) {
           >
             <div className={styles['content']}>
               <div>
-                {<img src={item.value == 'pytorch' ? pytorch : tensorflow} />}
+                {<img height={35} src={item.value == 'pytorch' ? pytorch : tensorflow} />}
               </div>
 
               <Checkbox
