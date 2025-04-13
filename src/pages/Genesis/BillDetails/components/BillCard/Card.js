f772456d24ab1151ff02eb63e6948cba343410a0
EdondjiIbolo <enzombul@gmail.com>
2025-04-13 19:58:00 +0800

    billstable interface updated

---
+++ src/pages/Genesis/BillDetails/components/BillCard/Card.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import styles from './index.less';
+import numeral from 'numeral';
+import PieChart from '../Pie/Pie';
+export default function Card({ total }) {
+  const unit = 'veJCT';
+  return (
+    <div className={styles['total-wrapper']}>
+      <div className={styles['total-context']}>
+        <span className={styles['total-text']}>Total cost </span>
+        <span className={[styles['value'], styles['total-value']].join(' ')}>
+          {`${numeral(total?.sum).format('0.00')} `}
+          <span className={styles['currency']}>{unit}</span>
+        </span>
+      </div>
+      <div className={styles['cols']}>
+        <span className={styles['equal']}>=</span>
+        <div className={styles['col-left']}>
+          <div>
+            <span className={`${styles['desc']} ${styles['blue']}`}>
+              {' '}
+              Cash payment{' '}
+            </span>
+            <span className={styles['value']}>{`${numeral(total?.cash).format(
+              '0.00',
+            )} ${unit}`}</span>
+          </div>
+          <div>
+            <span className={`${styles['desc']} ${styles['red']}`}>
+              Share bonus
+            </span>
+            <span className={styles['value']}>{`${numeral(total?.share).format(
+              '0.00',
+            )} ${unit}`}</span>
+          </div>
+        </div>
+        <span className={styles['equal']}>+</span>
+        <div className={styles['col-right']}>
+          <div>
+            <span className={`${styles['desc']} ${styles['yellow']}`}>
+              {' '}
+              Gift money{' '}
+            </span>
+            <span className={styles['value']}>{`${numeral(total?.gift).format(
+              '0.00',
+            )} ${unit}`}</span>
+          </div>
+          <div>
+            <span className={`${styles['desc']} ${styles['cyan']}`}>
+              Coupon
+            </span>
+            <span className={styles['value']}>{`${numeral(total?.coupon).format(
+              '0.00',
+            )} ${unit}`}</span>
+          </div>
+        </div>
+      </div>
+      <div>
+        <PieChart className={styles['chart']} total={total} />
+      </div>
+    </div>
+  );
+}
---
+++ src/pages/Genesis/BillDetails/components/Pie/Pie.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import ReactECharts from 'echarts-for-react';
+
+const PieChart = ({ total }) => {
+  const option = {
+    tooltip: {
+      trigger: 'item',
+    },
+    graphic: {
+      type: 'text',
+      left: 'center',
+      top: 'center',
+      style: {
+        text: 'Total cost',
+        textAlign: 'center',
+        fill: '#fff',
+        fontSize: 16,
+        fontWeight: 'bold',
+      },
+    },
+    series: [
+      {
+        name: 'Access From',
+        type: 'pie',
+        radius: ['70%', '90%'], // donut shape
+        avoidLabelOverlap: false,
+        label: {
+          show: false,
+          position: 'center',
+        },
+        emphasis: {
+          label: {
+            show: true,
+            fontSize: 10,
+            fontWeight: 'bold',
+          },
+        },
+        labelLine: {
+          show: false,
+        },
+        data: [
+          {
+            value: total?.cash,
+            name: 'Cash payment',
+            itemStyle: { color: 'blue' },
+          },
+          {
+            value: total?.share,
+            name: 'Share bonus',
+            itemStyle: { color: 'red' },
+          },
+          {
+            value: total?.gift,
+            name: 'Gift money',
+            itemStyle: { color: 'yellow' },
+          },
+          {
+            value: total?.coupon,
+            name: 'Coupon',
+            itemStyle: { color: 'cyan' },
+          },
+        ],
+      },
+    ],
+  };
+
+  return (
+    <ReactECharts option={option} style={{ height: '400px', width: '200px' }} />
+  );
+};
+
+export default PieChart;
