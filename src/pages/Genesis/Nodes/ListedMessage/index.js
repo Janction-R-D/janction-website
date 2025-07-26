3f545f71f42952248974f08919fd34cb3565b473
monomoskin <marcosmbul@gmail.com>
2025-07-27 02:57:26 +0800

    listed sms notification component added

---
+++ src/pages/Genesis/Nodes/ListedMessage/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from './index.less';
+import { history } from 'umi';
+export default function ListedMessage() {
+  return (
+    <div className={styles['warning-box']}>
+      <i className="iconfont icon-info" />
+      <span>
+        Please bind your email to receive real-time node monitoring updates
+        <span
+          className={styles['bind']}
+          onClick={() => history.push('/genesis/user-center')}
+        >
+          Go bind email.
+        </span>
+      </span>
+    </div>
+  );
+}
