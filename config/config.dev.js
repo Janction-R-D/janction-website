e2570db6693359fd774c95c71263518ca9d6e6e6
monomoskin <marcosmbul@gmail.com>
2025-08-01 20:58:12 +0800

    refreshing code

--- config/config.dev.js
+++ config/config.dev.js
@@ -4,8 +4,8 @@ export default defineConfig({
   define: {
     'process.env': {
       JANCTION_ENV: 'development',
-      JANCTION_API: '/api',
-      JANCTION_V0_API: '/v0',
+      JANCTION_API: 'https:/dev.janction.ai/api',
+      JANCTION_V0_API: 'https:/dev.janction.ai/v0',
       JANCTION_BASE_API: 'https://dev.janction.ai',
       JANCTION_SOCKET_API: 'wss://dev.janction.ai',
       ASSETS_URL: 'https://assets.janction.ai',
