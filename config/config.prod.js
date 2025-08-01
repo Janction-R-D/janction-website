be8aa3f826714855372958cb008308a67246b73f
GretchenLeee <lyf1990s@gmail.com>
2025-08-01 18:31:50 +0800

    fix: proxy

--- config/config.prod.js
+++ config/config.prod.js
@@ -4,8 +4,8 @@ export default defineConfig({
   define: {
     'process.env': {
       JANCTION_ENV: 'production',
-      JANCTION_API: 'https://www.janction.ai/api',
-      JANCTION_V0_API: 'https://www.janction.ai/v0',
+      JANCTION_API: '/api',
+      JANCTION_V0_API: '/v0',
       JANCTION_BASE_API: 'https://www.janction.ai',
       JANCTION_SOCKET_API: 'wss://www.janction.ai',
       ASSETS_URL: 'https://assets.janction.ai',
