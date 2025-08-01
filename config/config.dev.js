65cf527433a47b44358b17dc3d348c19b30585e8
monomoskin <marcosmbul@gmail.com>
2025-08-01 21:31:48 +0800

    config reseted

--- config/config.dev.js
+++ config/config.dev.js
@@ -4,7 +4,7 @@ export default defineConfig({
   define: {
     'process.env': {
       JANCTION_ENV: 'development',
-      JANCTION_API: 'https:/dev.janction.ai/api',
+      JANCTION_API: 'https://dev.janction.ai/api',
       JANCTION_V0_API: 'https:/dev.janction.ai/v0',
       JANCTION_BASE_API: 'https://dev.janction.ai',
       JANCTION_SOCKET_API: 'wss://dev.janction.ai',
