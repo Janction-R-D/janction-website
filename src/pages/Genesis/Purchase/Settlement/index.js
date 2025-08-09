5e60a6090d99708b3d9c8a260e4652e3611c098a
monomoskin <marcosmbul@gmail.com>
2025-08-10 01:28:13 +0800

    resource tunnel and order gpu info fixed

--- src/pages/Genesis/Purchase/Settlement/index.js
+++ src/pages/Genesis/Purchase/Settlement/index.js
@@ -156,7 +156,7 @@ const Settlement = (props) => {
 
       const payload = {
         node_id: node?.id,
-        tempalte: template || 'base',
+        template: template || 'base',
         purchase_instance_quantity: 1,
         purchase_duration: value,
         purchase_duration_unit: goal?.label.toLowerCase(),
