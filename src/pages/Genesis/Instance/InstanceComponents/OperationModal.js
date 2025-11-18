1b893da65f7c7ce0fece72d3b6265e0e79bb8e03
Nash <nashljy@icloud.com>
2025-11-18 18:17:31 +0800

    update code

--- src/pages/Genesis/Instance/InstanceComponents/OperationModal.js
+++ src/pages/Genesis/Instance/InstanceComponents/OperationModal.js
@@ -35,8 +35,6 @@ export default function OperationModal({ record, getAllNodes }) {
   const [selectValue, setSelectValue] = useState(undefined);
   const chainId = useChainId();
   const signer = useEthersSigner(chainId);
-  console.log(signer, 'signer');
-  console.log(222222, 'chainId', chainId);
   const [modalVisible, setModalVisible] = useState(false);
 
   const openModal = () => setModalVisible(true);
@@ -359,7 +357,7 @@ export default function OperationModal({ record, getAllNodes }) {
       >
         <a>
           {intl.formatMessage({ id: 'more.functions' })}
-          {'333333'}
+          {'   '}
           <i className="iconfont icon-down" />
         </a>
       </JanctionPopover>
